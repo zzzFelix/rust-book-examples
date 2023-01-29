f32a23e972dfc412ae7c51c15a5f2f9245c56c1d
Felix Hoffmann <felix.hoffmann@iteratec.com>
2023-01-29 22:27:20 +0100

    test: fibonacci

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -2,7 +2,7 @@ use rug::Integer;
 use std::io;
 
 fn main() {
-    let fibonacci: [Integer; 2] = [Integer::from(1), Integer::from(1)];
+    let fibonacci: [Integer; 2] = [Integer::from(0), Integer::from(1)];
     let n = input_n();
     let result = calculate_fibonacci(n, fibonacci);
     println!("nth Fibonacci number: {result}");
@@ -10,13 +10,13 @@ fn main() {
 
 fn calculate_fibonacci(n: usize, memo: [Integer; 2]) -> Integer {
     match n {
-        0..=2 => handle_edge_cases(n),
+        0..=1 => handle_edge_cases(n),
         n => handle_default_cases(n, memo),
     }
 }
 
 fn handle_default_cases(n: usize, mut memo: [Integer; 2]) -> Integer {
-    let mut i = 3;
+    let mut i = 2;
     let mut result: Integer = (&memo[0] + &memo[1]).into();
     while i <= n {
         result = (&memo[0] + &memo[1]).into();
@@ -31,7 +31,6 @@ fn handle_edge_cases(n: usize) -> Integer {
     match n {
         0 => Integer::from(0),
         1 => Integer::from(1),
-        2 => Integer::from(1),
         _ => panic!("This cannot happen"),
     }
 }
@@ -52,4 +51,40 @@ fn input_n() -> usize {
 fn retry_input() -> usize {
     println!("Not a valid number. Try again!");
     input_n()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zero_fibonacci() {
+        let n = 0;
+        let result = handle_edge_cases(n);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn first_fibonacci() {
+        let n = 1;
+        let result = handle_edge_cases(n);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn fifth_fibonacci() {
+        let memo: [Integer; 2] = [Integer::from(0), Integer::from(1)];
+        let n = 5;
+        let result = handle_default_cases(n, memo);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn millionth_fibonacci() {
+        let memo: [Integer; 2] = [Integer::from(0), Integer::from(1)];
+        let n = 1000000;
+        let result = handle_default_cases(n, memo);
+        let result_as_string = &result.to_string_radix(10)[..10];
+        assert_eq!(result_as_string, String::from("1953282128"));
+    }
 }
