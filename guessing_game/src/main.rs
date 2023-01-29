cf5cec2641a0740c514e55fb59ac23ed905a69b2
Felix Hoffmann <felix.hoffmann@iteratec.com>
2023-01-29 12:41:26 +0100

    chore: lint and format

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,7 @@
-use std::io;
-use std::cmp::Ordering;
-use std::num::ParseIntError;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::num::ParseIntError;
 
 fn main() {
     let secret_number: u8 = generate_secret_number();
@@ -12,7 +12,7 @@ fn main() {
     loop {
         let guess: u8 = match input_guess() {
             Ok(num) => num,
-            Err(_) => continue
+            Err(_) => continue,
         };
         give_feedback(&guess, &secret_number);
         if is_correct_guess(&guess, &secret_number) {
@@ -28,22 +28,22 @@ fn generate_secret_number() -> u8 {
 }
 
 fn give_feedback(guess: &u8, secret_number: &u8) {
-    match &guess.cmp(&secret_number) {
+    match &guess.cmp(secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        Ordering::Equal => println!("You win!"),
     }
 }
 
 fn is_correct_guess(guess: &u8, secret_number: &u8) -> bool {
-    return guess == secret_number;
+    guess == secret_number
 }
 
 fn input_guess() -> Result<u8, ParseIntError> {
     let mut guess: String = String::new();
 
     io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        .read_line(&mut guess)
+        .expect("Failed to read line");
     guess.trim().parse()
 }
