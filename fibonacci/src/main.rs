50e99be786b897efdd16c8174d8101f529e545fa
Felix Hoffmann <felix.hoffmann@iteratec.com>
2023-01-29 18:28:19 +0100

    feat: limit input to 1,000,000
    Prevent overheating CPUs🧯

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -36,12 +36,12 @@ fn handle_edge_cases(n: usize) -> Integer {
 }
 
 fn input_n() -> usize {
-    println!("Please input a number between 1 and 1000000000");
+    println!("Please input a number between 1 and 1000000");
     let mut n = String::new();
     io::stdin().read_line(&mut n).expect("Not a valid input");
     match n.trim().parse() {
         Ok(n) => match n {
-            1..=1000000000 => n,
+            1..=1000000 => n,
             _ => retry_input(),
         },
         Err(_) => retry_input(),
